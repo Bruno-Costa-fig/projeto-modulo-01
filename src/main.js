9c9e5ec86c09269f9bfb4e7cfde779deda8bc956
Bruno-Costa-fig <brunocosta.fig@gmail.com>
2023-08-31 18:59:37 -0300

    feat: formulario usando vuetify

--- src/main.js
+++ src/main.js
@@ -4,8 +4,23 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi'
+  },
+})
+
 const app = createApp(App)
 
 app.use(router)
+app.use(vuetify)
 
 app.mount('#app')
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,70 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-form ref="form" @submit.prevent="handleSubmit">
+    <v-text-field 
+      variant="outlined" 
+      v-model="aluno.nome" 
+      :rules="[value => !!value || 'O nome é obrigatório']"
+      type="text" 
+      label="Nome" 
+      placeholder="Digite o nome" 
+    />
+    
+    <v-text-field 
+      variant="outlined" 
+      v-model="aluno.idade" 
+      :rules="[value => value > 10 || 'A idade mínima é 10 anos',
+      value => value < 100 || 'A idade máxima é 100 anos']"
+      type="number" 
+      label="Idade" 
+      placeholder="Digite a idade"
+    />
+
+    <v-text-field 
+      variant="outlined"
+     v-model="aluno.email" 
+     type="email" 
+     label="Email" 
+     placeholder="Digite o email" 
+     />
+
+    <v-select 
+      :items="items" 
+      v-model="aluno.curso" 
+      variant="outlined" 
+      label="Curso"
+      placeholder="Selecione o curso">
+    </v-select>
+    <v-btn type="submit">Cadastrar</v-btn>
+  </v-form>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
+<script>
+export default {
+  data() {
+    return {
+      aluno: {
+        nome: "",
+        idade: null,
+        email: "",
+        curso: ""
+      },
+      items: ['HTML', 'CSS', 'JAVASCRIPT', 'VUEJS', 'PHP']
+    }
+  },
+  methods: {
+    async handleSubmit(){
+      const {valid} = await this.$refs.form.validate()
+
+      if(!valid){
+        alert("Preencha todos os dados!")
+        return
+      }
+
+      const result = confirm("Aluno cadastrado com sucesso! Deseja ir para o dashboard?")
+      this.$refs.form.reset()
+    }
   }
 }
-</style>
+</script>
+
+<style></style>
